8720c92e634a6c5f85c05ae1c676f193a6bdf876
Rodrigo da Silva Correia <rodrigocorreia@MacBook-Pro-de-Rodrigo.local>
2021-08-20 14:33:04 -0300

    Using hooks

--- my-app/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ my-app/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, TextField, Switch, FormControlLabel } from "@material-ui/core";
 
 function FormularioCadastro() {
+  const [nome, setNome] = useState("");
+  const [sobrenome, setSobrenome] = useState("");
+  const [cpf, setCfp] = useState("");
+
   return (
     <form>
       <TextField
@@ -10,6 +14,14 @@ function FormularioCadastro() {
         variant="outlined"
         fullWidth
         margin="normal"
+        value={nome}
+        onChange={(event) => {
+          let tmpNome = event.target.value;
+          if (tmpNome.length >= 12) {
+            tmpNome = tmpNome.substr(0, 12);
+          }
+          setNome(tmpNome);
+        }}
       />
       <TextField
         id="sobrenome"
@@ -17,6 +29,14 @@ function FormularioCadastro() {
         variant="outlined"
         fullWidth
         margin="normal"
+        value={sobrenome}
+        onChange={(event) => {
+          let tmpSobrenome = event.target.value;
+          if (tmpSobrenome.length >= 50) {
+            tmpSobrenome = tmpSobrenome.substr(0, 50);
+          }
+          setSobrenome(tmpSobrenome);
+        }}
       />
       <TextField
         id="cpf"
@@ -24,6 +44,14 @@ function FormularioCadastro() {
         variant="outlined"
         fullWidth
         margin="normal"
+        value={cpf}
+        onChange={(event) => {
+          let tmpCpf = event.target.value;
+          if (tmpCpf.length >= 11) {
+            tmpCpf = tmpCpf.substr(0, 11);
+          }
+          setCfp(tmpCpf);
+        }}
       />
 
       <FormControlLabel
