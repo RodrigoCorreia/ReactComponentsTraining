d22c359daaf92067c7aaff8d7b488ad0bb9554a8
Rodrigo da Silva Correia <rodrigocorreia@MacBook-Pro-de-Rodrigo.local>
2021-08-20 12:40:42 -0300

    Buildins components with material ui

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,14 @@
-import { Fragment } from "react";
 import "./App.css";
 import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
+import { Container, Typography } from "@material-ui/core";
+import 'fontsource-roboto';
 
 function App() {
   return (
-    <Fragment>
-      <h1>Formulario</h1>
+    <Container component="article" maxWidth="sm">
+      <Typography variant="h3" component="h1" align="center">Formulário de cadastro</Typography>
       <FormularioCadastro />
-    </Fragment>
+    </Container>
   );
 }
 
--- my-app/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ my-app/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,9 +1,48 @@
 import React from "react";
+import { Button, TextField, Switch, FormControlLabel } from "@material-ui/core";
 
 function FormularioCadastro() {
   return (
     <form>
-      <input type="text"></input>
+      <TextField
+        id="nome"
+        label="Nome"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+      />
+      <TextField
+        id="sobrenome"
+        label="Sobrenome"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+      />
+      <TextField
+        id="cpf"
+        label="CPF"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+      />
+
+      <FormControlLabel
+        label="Promoções"
+        control={
+          <Switch name="promocoes" color="primary" defaultChecked></Switch>
+        }
+      ></FormControlLabel>
+
+      <FormControlLabel
+        label="Novidades"
+        control={
+          <Switch name="novidades" color="primary" defaultChecked></Switch>
+        }
+      ></FormControlLabel>
+
+      <Button type="submit" variant="contained" color="primary">
+        Cadastrar
+      </Button>
     </form>
   );
 }
